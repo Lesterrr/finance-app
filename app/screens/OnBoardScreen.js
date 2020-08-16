badccca1d47dacefae8cfce105d8304fd2d7c2a7
Lester <lester.deguzman@tup.edu.ph>
2020-08-16 10:51:27 +0800

    Add Swiper on OnBoardingScreen

--- app/screens/OnBoardScreen.js
+++ app/screens/OnBoardScreen.js
@@ -1,31 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Image, Text } from "react-native";
+import Swiper from "react-native-swiper";
 
 import Button from "../components/Button";
 
 const OnBoardScreen = () => {
+  const [showPagination, setShowPagination] = useState(true);
+
   return (
-    <React.Fragment>
+    <Swiper
+      loop={false}
+      showsPagination={showPagination}
+      onIndexChanged={(index) => index === 2 && setShowPagination(false)}
+      scrollEnabled={showPagination}
+    >
       <View style={styles.slide}>
         <Image
           style={styles.image}
-          source={require("../assets/onBoard/teaching-bw.png")}
+          source={require("../assets/onBoard/save-money-bw.png")}
         />
         <View style={styles.textContainer}>
-          <Text style={styles.title}>This is a title</Text>
+          <Text style={styles.title}>Manage your savings digitally</Text>
           <Text style={styles.description}>
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua."
           </Text>
         </View>
       </View>
-      <View style={styles.button}>
-        <Button
-          title="Get Started"
-          onPress={() => console.log("Skip button was pressed!")}
+      <View style={styles.slide}>
+        <Image
+          style={styles.image}
+          source={require("../assets/onBoard/analyse-bw.png")}
         />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>keep track on your spending habits</Text>
+          <Text style={styles.description}>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          </Text>
+        </View>
       </View>
-    </React.Fragment>
+      <View style={styles.slide}>
+        <Image
+          style={styles.image}
+          source={require("../assets/onBoard/teaching-bw.png")}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Learn how to grow your money</Text>
+          <Text style={styles.description}>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          </Text>
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Get Started"
+            onPress={() => console.log("Get Started button was pressed!")}
+          />
+        </View>
+      </View>
+    </Swiper>
   );
 };
 
@@ -48,7 +82,7 @@ const styles = StyleSheet.create({
   },
   title: {
     textAlign: "center",
-    fontSize: 30,
+    fontSize: 25,
   },
   description: {
     color: "grey",
@@ -56,5 +90,8 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: "#fff",
+    width: 250,
+    position: "absolute",
+    bottom: 20,
   },
 });
