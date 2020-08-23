f701e41eeb50bef87dce9e518e2ce05dcd2eaf5a
Lester <lester.deguzman@tup.edu.ph>
2020-08-23 16:00:03 +0800

    Add menu placeholder

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,41 +1,73 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList, ScrollView } from "react-native";
 import { connect } from "react-redux";
 
 import WalletItem from "../components/wallet/WalletItem";
+import Text from "../components/Text";
+import ActivityItem from "../components/activity/ActivityItem";
 
-const HomeScreen = ({ navigation, balance, totalIncome, totalExpenses }) => {
+const HomeScreen = ({
+  navigation,
+  balance,
+  totalIncome,
+  totalExpenses,
+  activity,
+}) => {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <WalletItem
-          title="Balance"
-          description={balance}
-          onPress={() => navigation.navigate("Activity")}
-          descriptionStyle={{ fontSize: 45 }}
-          containerStyle={{ alignItems: "center" }}
+      <View style={styles.walletContainer}>
+        <View style={styles.main}>
+          <WalletItem
+            title="Balance"
+            description={balance}
+            onPress={() => navigation.navigate("Activity")}
+            descriptionStyle={{ fontSize: 45 }}
+            containerStyle={{ alignItems: "center" }}
+          />
+        </View>
+        <View style={styles.secondary}>
+          <View style={styles.secondaryItem}>
+            <WalletItem
+              title="Total Income"
+              description={totalIncome}
+              onPress={() => navigation.navigate("Activity", { key: "income" })}
+              mode="income"
+              descriptionStyle={{ fontSize: 20, textAlign: "center" }}
+            />
+          </View>
+          <View style={styles.secondaryItem}>
+            <WalletItem
+              title="Total Expenses"
+              description={totalExpenses}
+              onPress={() =>
+                navigation.navigate("Activity", { key: "expense" })
+              }
+              mode="expense"
+              descriptionStyle={{ fontSize: 20, textAlign: "center" }}
+            />
+          </View>
+        </View>
+      </View>
+      <View style={styles.activityContainer}>
+        <Text>Transactions:</Text>
+        <FlatList
+          data={activity}
+          renderItem={({ item }) => <ActivityItem {...item} />}
+          keyExtractor={(_, index) => index.toString()}
         />
       </View>
-      <View style={styles.secondary}>
-        <View style={styles.secondaryItem}>
-          <WalletItem
-            title="Total Income"
-            description={totalIncome}
-            onPress={() => navigation.navigate("Activity", { key: "income" })}
-            mode="income"
-            descriptionStyle={{ fontSize: 20, textAlign: "center" }}
-          />
-        </View>
-        <View style={styles.secondaryItem}>
-          <WalletItem
-            title="Total Expenses"
-            description={totalExpenses}
-            onPress={() => navigation.navigate("Activity", { key: "expense" })}
-            mode="expense"
-            descriptionStyle={{ fontSize: 20, textAlign: "center" }}
-          />
-        </View>
-      </View>
+      <ScrollView style={styles.menu} horizontal={true}>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+        <View style={styles.menuItem}></View>
+      </ScrollView>
     </View>
   );
 };
@@ -43,8 +75,7 @@ const HomeScreen = ({ navigation, balance, totalIncome, totalExpenses }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30,
-    alignItems: "center",
+    backgroundColor: "#fff",
   },
   main: {
     height: 150,
@@ -57,12 +88,45 @@ const styles = StyleSheet.create({
   },
   secondaryItem: {
     width: "50%",
+    height: 50,
     alignItems: "center",
+  },
+  walletContainer: {
+    paddingTop: 30,
+    paddingBottom: 50,
+    alignItems: "center",
+    backgroundColor: "#fdb32a",
+  },
+  activityContainer: {
+    width: "90%",
+    height: 250,
+    borderRadius: 6,
+    elevation: 3,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    backgroundColor: "white",
+    alignSelf: "center",
+    marginHorizontal: 10,
+    top: -30,
+    padding: 10,
+  },
+  menu: {
+    top: -12,
+    marginHorizontal: 15,
+  },
+  menuItem: {
+    width: 90,
+    height: 120,
+    marginHorizontal: 5,
+    borderRadius: 20,
+    backgroundColor: "#fdb32a",
   },
 });
 
 const mapStateToProps = (state) => {
   return {
+    activity: state.wallet.activity,
     balance: state.wallet.balance,
     totalIncome: state.wallet.totalIncome,
     totalExpenses: state.wallet.totalExpenses,
