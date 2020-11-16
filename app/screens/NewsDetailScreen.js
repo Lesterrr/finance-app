928443a7a69efc8d453e8275b356d04bb0813504
Lester016 <lesterdeguzman1999@gmail.com>
2020-11-16 18:14:25 +0800

    Add dummy text in NewsDetailScreen

--- app/screens/NewsDetailScreen.js
+++ app/screens/NewsDetailScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import * as WebBrowser from "expo-web-browser";
 
 import colors from "../config/colors";
 
@@ -15,16 +23,32 @@ const NewsDetailScreen = ({ route }) => {
     content,
     onPress,
   } = route.params;
+
+  // Links will be open using app browser.
+  const handleLinkOpen = () => {
+    WebBrowser.openBrowserAsync(url);
+  };
+
   return (
     <ScrollView>
       {/* Image */}
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: urlToImage }} />
-      </View>
-      {/* Texts */}
+      {urlToImage && (
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{ uri: urlToImage }} />
+        </View>
+      )}
 
+      {/* Texts */}
       <View style={styles.textContainer}>
-        <Text style={styles.title}>{title}</Text>
+        <TouchableOpacity onPress={handleLinkOpen}>
+          <Text
+            style={
+              urlToImage ? [styles.title] : [styles.title, { marginTop: 50 }]
+            }
+          >
+            {title}
+          </Text>
+        </TouchableOpacity>
         <View style={styles.subTitle}>
           <Text style={styles.subTitleText}>{author}</Text>
           <Text style={styles.subTitleText}>{publishedAt}</Text>
@@ -32,6 +56,15 @@ const NewsDetailScreen = ({ route }) => {
       </View>
       <View style={styles.content}>
         <Text style={styles.contentText}>{content}</Text>
+        <Text style={styles.contentText}>
+          {"\n"}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
       </View>
     </ScrollView>
   );
@@ -48,7 +81,7 @@ const styles = StyleSheet.create({
     padding: 25,
   },
   title: {
-    fontSize: 22,
+    fontSize: 18,
     fontWeight: "bold",
     color: colors.primary,
   },
@@ -62,6 +95,7 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 25,
+    paddingTop: 0,
   },
   contentText: {
     color: colors.medium,
